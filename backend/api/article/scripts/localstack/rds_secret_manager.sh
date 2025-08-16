c5ba5ed5d086db7f30911373d9fcbd31ce2278dc
tamaco489 <tamaco489@gmail.com>
2025-08-17 03:35:39 +0900

    feature/backend/setup (#20)
    * feat: add environment configuration files for PostgreSQL and API server
    
    * feat: add .gitignore for backend API to exclude temporary files and environment variables
    
    * feat: add rds_secret_manager script to create secrets in localstack
    
    * fix: update POSTGRES_DB to 'core' in .env_localstack_sample and add .keep file
    
    * feat: add docker-compose configuration for API, PostgreSQL, and LocalStack services
    
    * feat: add Makefile for environment setup, API generation, and datastore operations
    
    * feat: initialize main application and module for article API
    
    * fix: update healthcheck endpoint URL in Makefile
    
    * feat: add .air.toml configuration for article API build and logging
    fix: update Makefile to start localstack service during rebuild
    
    * feat: add .gitkeep files for article API internal directories
    
    * feat: バックエンドのディレクトリ構成と開発ルールに関するドキュメントを追加

---
+++ backend/api/article/scripts/localstack/rds_secret_manager.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Load values from .env_localstack
+set -a # Enable automatic export of environment variables
+source /etc/localstack/init/ready.d/.env_localstack
+set +a # Disable automatic export of environment variables
+
+# Create a secret in Secrets Manager
+awslocal secretsmanager create-secret \
+  --name 'dev/tamaco-blog/article/core/rds' \
+  --region ap-northeast-1 \
+  --secret-string "{
+    \"username\":\"${POSTGRES_USER}\",
+    \"password\":\"${POSTGRES_PASSWORD}\",
+    \"host\":\"${POSTGRES_HOST}\",
+    \"port\":\"${POSTGRES_PORT}\",
+    \"dbname\":\"${POSTGRES_DB}\"
+  }"
